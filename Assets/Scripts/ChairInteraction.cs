324c60de7e2748db709a3947722cd18bd5565c53
MarcusJames0930 <marcus.james19@yahoo.com>
2023-01-23 14:53:25 -0500

    adding new puzzle

---
+++ Assets/Scripts/ChairInteraction.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SafeInteract : MonoBehaviour
+{
+    private Color initialColor;
+    public bool noEmissionAtStart = true;
+    public Color highlightColor = Color.red;
+    private bool mouseon = false;
+    private Renderer myRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRenderer = GetComponent<Renderer>();
+        if (noEmissionAtStart)
+            initialColor = Color.black;
+        else
+            initialColor = myRenderer.material.GetColor("_EmissionColor");
+    }
+
+    void OnMouseEnter()
+    {
+        mouseon = true;
+        myRenderer.material.SetColor("_EmissionColor", highlightColor);
+
+    }
+
+    void OnMouseExit()
+    {
+        mouseon = false;
+        myRenderer.material.SetColor("_EmissionColor", initialColor);
+    }
+
+    // Update is called once per frame
+    void Update2()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (mouseon == true)
+            {
+                SceneManager.LoadScene("ChairPuzzle");
+            }
+        }
+
+    }
+}
+
