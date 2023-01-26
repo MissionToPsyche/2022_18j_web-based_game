eb480eec38c1784baf4fb392f97a662efb9e31d9
MarcusJames0930 <marcus.james19@yahoo.com>
2023-01-26 11:33:46 -0500

    added new script
    added new script

--- Assets/Scripts/Office/Chair/ChairPuzzleEntry.cs
+++ Assets/Scripts/Office/Chair/ChairPuzzleEntry.cs
@@ -5,46 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class ChairPuzzleEntry : MonoBehaviour
 {
-    private Color initialColor;
-    public bool noEmissionAtStart = true;
-    public Color highlightColor = Color.red;
-    private bool mouseon = false;
-    private Renderer myRenderer;
-
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter(Collider other)
     {
-        myRenderer = GetComponent<Renderer>();
-        if (noEmissionAtStart)
-            initialColor = Color.black;
-        else
-            initialColor = myRenderer.material.GetColor("_EmissionColor");
+        SceneManager.LoadScene("ChairPuzzle");
     }
 
-    void OnMouseEnter()
-    {
-        mouseon = true;
-        myRenderer.material.SetColor("_EmissionColor", highlightColor);
-
-    }
-
-    void OnMouseExit()
-    {
-        mouseon = false;
-        myRenderer.material.SetColor("_EmissionColor", initialColor);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            if (mouseon == true)
-            {
-                SceneManager.LoadScene("ChairPuzzle");
-            }
-        }
-
-    }
 }
 
