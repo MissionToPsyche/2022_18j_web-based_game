c7bdabb4e3b921b48c029c569f9237c23fd9a232
MarcusJames0930 <marcus.james19@yahoo.com>
2023-02-02 15:14:38 -0500

    added maze design

--- Assets/Scripts/Office/MazeObjectMovement.cs
+++ Assets/Scripts/Office/MazeObjectMovement.cs
@@ -5,13 +5,16 @@ public class MazeObjectMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
         float xDirection = Input.GetAxis("Horizontal");
         float yDirection = Input.GetAxis("Vertical");
 
         Vector3 moveDirection = new Vector3(xDirection, 0.0f, yDirection);
 
         transform.position += moveDirection;
+    }
+
+    void fixedUpdate()
+    {
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
